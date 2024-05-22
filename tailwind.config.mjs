b72a329e0cc562439c484420d84bd1373363915a
Amets <ametsmarti7@gmail.com>
2024-05-22 16:10:59 +0200

    asd

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import react from '@astrojs/react';
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -6,5 +7,10 @@ export default {
 	},
 	plugins: [
 		require('tailwind-scrollbar-hide')
+	],
+	integrations: [
+		react({
+		  include: ['**/react/*'],
+		}),
 	],
 }
