8116dc459f24262cdc5c9ad73b26fd3e328dbfe2
Amets <ametsmarti7@gmail.com>
2024-06-12 12:44:01 +0200

    try 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  base: '/Astro/',
+  site:'https://AmetsMarti.github.io',
 });
