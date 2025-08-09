fe782e53d193bc3efc2e60b16cb116a081c1a472
Amets <amartiarena017@gmail.com>
2025-08-09 13:22:59 +0200

    Fixed site domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ametsmartui.github.io/Astro', // root domain
+  site: 'https://ametsmartui.github.io/Astro/', // root domain
   integrations: [tailwind(), react(), icon(), astroI18next()],
   i18n: {
     defaultLocale: "es",
