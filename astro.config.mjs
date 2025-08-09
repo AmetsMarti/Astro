7cc02979a8bc3caca47f6f9a18bfaff887191b04
Amets <amartiarena017@gmail.com>
2025-08-09 13:34:02 +0200

    test with base name

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ametsmartui.github.io/Astro/', // root domain
+  site: 'https://ametsmarti.github.io/Astro', // root domain
+  base: '/Astro/', // repository name
   integrations: [tailwind(), react(), icon(), astroI18next()],
   i18n: {
     defaultLocale: "es",
